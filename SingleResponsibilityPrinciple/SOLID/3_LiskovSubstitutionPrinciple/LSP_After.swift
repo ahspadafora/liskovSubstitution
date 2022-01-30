69b35b9b6f0d270872c008b2851542727bdc4ec0
Amber Spadafora <ahelenespadafora@gmail.com>
2022-01-29 20:08:15 -0500

    Example of LSP

--- SingleResponsibilityPrinciple/SOLID/3_LiskovSubstitutionPrinciple/LSP_After.swift
+++ SingleResponsibilityPrinciple/SOLID/3_LiskovSubstitutionPrinciple/LSP_After.swift
@@ -17,3 +17,50 @@
 
 import Foundation
 
+fileprivate protocol Polygon {
+    func calculateArea() -> Float
+}
+
+fileprivate class Rectangle: Polygon {
+    var width: Float = 0
+    var height: Float = 0
+    
+    func calculateArea() {
+        return width * height
+    }
+    
+    func set(width: Float) {
+        self.width = width
+    }
+    
+    func set(height: Float) {
+        self.height = height
+    }
+}
+
+fileprivate class Square: Polygon {
+    var side: Float = 0
+    
+    func set(side: Float) {
+        self.side = side
+    }
+    
+    func calculateArea() -> Float {
+        return pow(side, 2)
+    }
+}
+
+fileprivate func printArea(of polygon: Polygon) {
+    print(polygon.calculateArea)
+}
+
+fileprivate func example() {
+    let rectangle = Rectangle()
+    rectangle.set(width: 5)
+    rectangle.set(height: 4)
+    printArea(of: rectangle)
+    
+    let square = Square()
+    rectangle.set(side: 4)
+    printArea(of: square)
+}
--- SingleResponsibilityPrinciple/SOLID/3_LiskovSubstitutionPrinciple/LSP_Before.swift
+++ SingleResponsibilityPrinciple/SOLID/3_LiskovSubstitutionPrinciple/LSP_Before.swift
@@ -7,3 +7,50 @@
 
 import Foundation
 
+// Liskov Substitution Principle
+// Objects should be replaced with instances of their subclasses without altering their behavior
+
+fileprivate class Rectangle {
+    
+    var width: Float = 0
+    var height: Float = 0
+    
+    func set(width: Float) {
+        self.width = width
+    }
+    
+    func set(height: Float) {
+        self.height = height
+    }
+    
+    func calculateArea() -> Float {
+        return width * height
+    }
+}
+
+
+fileprivate class Square: Rectangle {
+    override set(width: Float) {
+        self.width = width
+        self.height = height
+    }
+    
+    override func set(height: Float) {
+        self.height = height
+        self.width = height
+    }
+}
+// breaks the LSP
+fileprivate func setSizeAndPrintArea(of rectangle: Rectangle) {
+    rectangle.set(height: 5)
+    rectangle.set(width: 4)
+    print(rectangle.calculateArea)
+}
+
+fileprivate func example() {
+    let rectangle = Rectangle()
+    setSizeAndPrintArea(of: rectangle) // prints 20
+    
+    let square = Square()
+    setSizeAndPrintArea(of: square) // print 16
+}
