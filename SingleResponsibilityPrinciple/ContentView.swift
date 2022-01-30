a69e3abe6c5cce5fb9823483b7893edd0d2d17ff
Amber Spadafora <ahelenespadafora@gmail.com>
2022-01-29 19:08:24 -0500

    Initial Commit

---
+++ SingleResponsibilityPrinciple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SingleResponsibilityPrinciple
+//
+//  Created by Amber Spadafora on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SingleResponsibilityPrinciple/SingleResponsibilityPrincipleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingleResponsibilityPrincipleApp.swift
+//  SingleResponsibilityPrinciple
+//
+//  Created by Amber Spadafora on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SingleResponsibilityPrincipleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
